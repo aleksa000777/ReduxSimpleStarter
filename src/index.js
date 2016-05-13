647ab44991ce075b1942cd95cf300fe1a17229f3
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-05-13 14:26:24 -0400

    set template

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = ()=>{
+const App = () => {
   return(
     <div>
-      Hi
+      <p>Here we are</p>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.querySelector('.container'))
